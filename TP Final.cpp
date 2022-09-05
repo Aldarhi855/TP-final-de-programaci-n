4a7dbfc3a7fa90e3aad177228d3318ed8f5736cd
Aldarhi855 <AldanaMoreyra@Outlook.com.ar>
2022-09-05 16:45:39 -0300

    creación del programa y algunas bases

---
+++ TP Final.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include <ctime>
+#include <cstdlib>
+#include <conio2.h>
+
+using namespace std;
+
+class Juego
+{
+protected:
+	int puntaje, *dirp;
+public:
+	void play();
+	Juego();
+};
+
+
+Juego::Juego()
+{
+	
+}
+
+void Juego:play()
+{
+	
+}
+
+
+int main (int argc, char *argv[]) 
+{
+	Juego p;
+	p.play();
+	
+	return 0;
+}
+
